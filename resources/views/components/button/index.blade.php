c22455f135c3fe9b188af1d10346b687496af4a0
Serge Bénard <serge.benard@gmail.com>
2023-07-19 11:35:27 -0400

    Add Active and Square modes for the button

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,7 +1,14 @@
 @props([
     'disabled' => false,
+    'active' => false,
+    'square' => false,
 ])
 
-<button {{ $attributes->class(['btn-disabled' => $disabled])->merge(['class' => 'btn', 'type' => 'button']) }} @disabled($disabled)>
+<button {{ $attributes->class([
+        'btn-active' => $active,
+        'btn-disabled opacity-60 cursor-not-allowed' => $disabled,
+        'btn' => !$square,
+        'btn-square' => $square,
+    ])->merge(['type' => 'button']) }} @disabled($disabled)>
     {{ $slot }}
 </button>
