2e3a80a009dbf72a1600300b58fdb245e969bd90
Serge Bénard <serge.benard@gmail.com>
2023-07-23 14:18:42 -0400

    Add a Blade date format helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Blade::directive('date', function (string $expression) {
+            // return Carbon::parse($expression)->locale( App::currentLocale() ) ;
+            return "<?php echo ($expression)->format('M-d-Y'); ?>";
+        });
     }
 }
